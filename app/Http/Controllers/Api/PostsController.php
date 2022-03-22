29a5395e5a61c9ca61d9d1863076556178d79fd8
matheusmartinsdev <fmatheusms@gmail.com>
2022-03-22 10:17:45 -0300

    first commit

---
+++ app/Http/Controllers/Api/PostsController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Orion\Http\Controllers\Controller;
+
+class ContratantesController extends Controller
+{
+    protected $model = Contratante::class;
+}
---
+++ routes/api.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Orion\Facades\Orion;
+use App\Http\Controllers\Api\PostsController;
+
+Route::group(['as' => 'api.'], function () {
+    Orion::resource('contratantes', ContratantesController::class);
+});
