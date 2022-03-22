5b7148c8819d1ae2fa8b64b3de39317bd21a49e6
matheusmartinsdev <fmatheusms@gmail.com>
2022-03-22 11:12:20 -0300

    Funcionario - model, controller e migration

---
+++ app/Http/Controllers/Api/FuncionariosController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Funcionario;
+use Orion\Http\Controllers\Controller;
+
+class FuncionarioController extends Controller
+{
+    protected $model = Funcionario::class;
+}
