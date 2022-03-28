a53a8372e2f25766f82ed4b6d340a17bcb88861b
matheusmartinsdev <fmatheusms@gmail.com>
2022-03-27 22:47:48 -0300

    rotas atualizadas

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,23 @@
 <?php
 
+use App\Http\Controllers\Api\ContratanteOrdensController;
+use App\Http\Controllers\Api\FuncionariosController;
+use App\Http\Controllers\Api\ContratantesController;
+use App\Http\Controllers\Api\OrdensServicoController;
 use Illuminate\Support\Facades\Route;
 use Orion\Facades\Orion;
-use App\Http\Controllers\Api\ContratantesController;
+use App\Http\Controllers\LoginController;
+use Illuminate\Support\Facades\Auth;
 
-Route::group(['as' => 'api.'], function () {
+Route::middleware('auth:sanctum')->group(function () {
+    // Devolver dados de usuário
+    Route::get("/eu", fn () => response()->json(Auth::user()));
 
-    // Trabalho em progreso...
+    Route::group(['as' => 'api.'], function () {
+        Orion::resource('/contratantes', ContratantesController::class)->except(["create", "edit"]);
+        Orion::resource('/servicos', OrdensServicoController::class)->except(["create", "edit"]);
+        Orion::resource('/funcionarios', FuncionariosController::class)->except(["create", "edit"]);
 
+        Orion::hasManyResource('/contratantes', '/servicos', ContratanteOrdensController::class)->except(["create", "edit"]);
+    });
 });
