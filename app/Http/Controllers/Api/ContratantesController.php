57283ba00c6418281debd1647e7bf9117d292c9b
matheusmartinsdev <fmatheusms@gmail.com>
2022-03-29 21:09:45 -0300

    Contratantes controller

--- app/Http/Controllers/Api/ContratantesController.php
+++ app/Http/Controllers/Api/ContratantesController.php
@@ -3,9 +3,12 @@
 namespace App\Http\Controllers\Api;
 
 use App\Models\Contratante;
+use Orion\Concerns\DisableAuthorization;
 use Orion\Http\Controllers\Controller;
 
 class ContratantesController extends Controller
 {
+    use DisableAuthorization;
+
     protected $model = Contratante::class;
 }
