542926c57ed54f53f3aa2761d825ed090e7d306a
Matheus Martins <fmatheusms@gmail.com>
2022-04-04 09:24:47 -0300

    teste cors

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie', '/login', '/logout', '/gestao-os-front'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', '/login', '/logout', '/gestao-os-front', '.gestao-os.matheusmartins.dev/*'],
 
     'allowed_methods' => ['*'],
 
