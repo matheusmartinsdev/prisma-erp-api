8414ce48c621b3cf865b348a691e35f5e9cda397
matheusmartinsdev <fmatheusms@gmail.com>
2022-05-04 12:41:06 -0300

    a

--- app/Http/Controllers/Api/OrdensServicoController.php
+++ app/Http/Controllers/Api/OrdensServicoController.php
@@ -3,9 +3,12 @@
 namespace App\Http\Controllers\Api;
 
 use App\Models\OrdemServico;
+use Orion\Concerns\DisableAuthorization;
 use Orion\Http\Controllers\Controller;
 
 class OrdensServicoController extends Controller
 {
+    use DisableAuthorization;
+
     protected $model = OrdemServico::class;
 }
--- routes/api.php
+++ routes/api.php
@@ -6,18 +6,49 @@ use App\Http\Controllers\Api\ContratantesController;
 use App\Http\Controllers\Api\OrdensServicoController;
 use Illuminate\Support\Facades\Route;
 use Orion\Facades\Orion;
-use App\Http\Controllers\LoginController;
+use App\Http\Resources\UserResource;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 Route::middleware('auth:sanctum')->group(function () {
     // Devolver dados de usuário
-    Route::get("/eu", fn () => response()->json(Auth::user()));
+    // Route::get("/eu", fn () => response()->json(Auth::user()->createToken('app-token')->plainTextToken));
 
     Route::group(['as' => 'api.'], function () {
         Orion::resource('/contratantes', ContratantesController::class)->except(["create", "edit"]);
-        Orion::resource('/servicos', OrdensServicoController::class)->except(["create", "edit"]);
+        Orion::resource('/ordens', OrdensServicoController::class)->except(["create", "edit"]);
         Orion::resource('/funcionarios', FuncionariosController::class)->except(["create", "edit"]);
 
         Orion::hasManyResource('/contratantes', '/servicos', ContratanteOrdensController::class)->except(["create", "edit"]);
     });
+
+    // Logout
+    Route::post("/logout", function (Request $request) {
+        if ($request->user()->tokens()->delete()) {
+            return response(status: 204);
+        }
+
+        return response()->json([
+            'message' => 'Internal Server Error!'
+        ], status: 500);
+    });
+});
+
+// Login
+Route::post("/login", function (Request $request) {
+    $credentials = $request->validate(
+        [
+            'email' => ['required', 'email'],
+            'password' => ['required']
+        ]
+    );
+
+    if (Auth::attempt($credentials)) {
+
+        $user = \App\Models\User::where('id', Auth::user()->id)->first();
+
+        return response()->json(new UserResource($user));
+    }
+
+    return response()->json(['message' => 'Credenciais inválidas'], status: 422);
 });
